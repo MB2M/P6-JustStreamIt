6e19a2d6e150d0410d6133ca6680d55788e2c25c
Damien Sabouraud <damsbooster@hotmail.com>
2021-01-17 00:28:26 +0100

    js import des tops films + catégories

--- public/js/main.js
+++ public/js/main.js
@@ -1,30 +1,53 @@
 
+function getFilmInfo(id, pre) {
+    var request = new XMLHttpRequest();
+    var response;
+    request.onreadystatechange = function(){
+            if (request.readyState == 4 && this.status == 200) {
+                response = JSON.parse(request.responseText);
+                document.getElementById(pre + '-title').innerHTML = response.title;
+                document.getElementById(pre + '-img').setAttribute('src',response.image_url);
+                document.getElementById(pre + '-description').innerHTML = response.long_description;
+            }
+        }
+        request.open("GET", "http://localhost:8000/api/v1/titles/" + id);
+        request.send();
+}
 
 
-
-
-function getScoreImdbMax(startingScore) {
+function filmTop(top, uri, category, slice_start, slice_end, films = []) {
         var request = new XMLHttpRequest();
         var response;
         request.onreadystatechange = function(){
             if (request.readyState == 4 && this.status == 200) {
                 response = JSON.parse(request.responseText);
-                if (response.count == 0) {
-                    startingScore = (startingScore * 10 - 1) / 10
-                    getScoreImdbMax(startingScore);
+                for (film of response.results){
+                    films.push([film.id, film.image_url])
                 }
-                if (response.count > 0){
-                    randomNumber = Math.floor((Math.random() * response.count));
-                    console.log(response.results[randomNumber]);
-                    document.getElementById('number-one-title').innerHTML = response.results[randomNumber].title;
-                    document.getElementById('number-one-img').setAttribute('src',response.results[randomNumber].image_url);
+                if (top-5 > 0){
+                    filmTop(top-5, response.next, category, slice_start, slice_end, films);
+                }
+                let all_img_HTML = "";
+                for (film of films.slice(slice_start, slice_end)) {
+                    const img_HTML = "<img id=\"" + film[0] + "\" src=\"" + film[1] + "\" class=\"carousel__img\"></img>";
+                    all_img_HTML = all_img_HTML + img_HTML;
+                    document.getElementById(category + "__carousel").innerHTML = all_img_HTML;
+                }
+                if (category == "top-film"){
+                    getFilmInfo(films[0][0], 'number-one');
                 }
             }
         }
-        request.open("GET", "http://localhost:8000/api/v1/titles/?imdb_score_min=" + startingScore);
+        request.open("GET", uri);
         request.send();
 
         return response;
     };
 
-getScoreImdbMax(10);
+const cat1 =  document.getElementById('category-1__title').textContent;
+const cat2 =  document.getElementById('category-2__title').textContent;
+const cat3 =  document.getElementById('category-3__title').textContent;
+filmTop(10, "http://localhost:8000/api/v1/titles/?sort_by=-imdb_score", 'top-film',1 ,8);
+filmTop(10, "http://localhost:8000/api/v1/titles/?sort_by=-imdb_score&genre_contains=" + cat1, 'category1', 0 , 7);
+filmTop(10, "http://localhost:8000/api/v1/titles/?sort_by=-imdb_score&genre_contains=" + cat2, 'category2', 0 , 7);
+filmTop(10, "http://localhost:8000/api/v1/titles/?sort_by=-imdb_score&genre_contains=" + cat3, 'category3', 0, 7);
